85936ca935a175e9813e62ef3c9517b79a66b24b
srikanth30 <kudikyalasrikanth@gmail.com>
2018-01-22 18:50:18 +0530

    item page

--- src/app/components/sub-categories/sub-categories.component.ts
+++ src/app/components/sub-categories/sub-categories.component.ts
@@ -17,9 +17,14 @@ export class SubCategoriesComponent implements OnInit {
   product: any;
   results: any;
   subcategories: any;
+  subcategory:any;
   cindex:any;
+  subcatname: any;
+  show: boolean= false;
+  itemdata:any;
 
   constructor(private router: Router,private route: ActivatedRoute,public http: HttpClient) {
+    this.subcategory = [];
     this.catid = this.route.snapshot.params['catid'];
     this.productid = this.route.snapshot.params['productid'];
     this.subcatid = this.route.snapshot.params['subcatid'];
@@ -28,13 +33,15 @@ export class SubCategoriesComponent implements OnInit {
     .subscribe(res => {
       this.results = res;
       this.product = this.results.data[this.productid-1];
+      this.subcatname = this.product.categories[this.cindex].sub_categories[0].name;
+      this.lengths(this.cindex);
       this.product.description ="assets/themes/images/carousel/"+this.product.name+".jpg";
     });
     this.http.get('http://13.127.87.1/index.php/rest/items/get/shop_id/?shop_id='+this.productid+'&sub_cat_id='+this.subcatid+'&item=true&count=all&from=0')
     .subscribe(res => {
       this.results = res;
       this.subcategories = this.results.data;
-      console.log("subcat",this.subcategories);
+      // console.log("subcat",this.subcategories);
       this.subcategories.forEach(element => {
         element.cover_image_file = "http://13.127.87.1/uploads/" + element.images[0].path;
       });
@@ -44,14 +51,17 @@ export class SubCategoriesComponent implements OnInit {
   }
 
   subCategory(element,index){
+    this.show = false;
     this.cindex = index;
+    this.lengths(this.cindex);
+    this.subcatname = this.product.categories[index].sub_categories[0].name;
     this.subcatid= element.sub_categories[0].id;
     this.catid = element.id;
     this.http.get('http://13.127.87.1/index.php/rest/items/get/shop_id/?shop_id='+this.productid+'&sub_cat_id='+this.subcatid+'&item=true&count=all&from=0')
     .subscribe(res => {
       this.results = res;
       this.subcategories = this.results.data;
-      console.log("subcat",this.subcategories);
+      // console.log("subcat",this.subcategories);
       this.subcategories.forEach(element => {
         element.cover_image_file = "http://13.127.87.1/uploads/" + element.images[0].path;
       });
@@ -59,14 +69,34 @@ export class SubCategoriesComponent implements OnInit {
   }
 
   swipe(element){
-    this.subcatid= element;
+    this.show = false;
+    this.subcatid= element.id;
+    this.subcatname = element.name;
     this.http.get('http://13.127.87.1/index.php/rest/items/get/shop_id/?shop_id='+this.productid+'&sub_cat_id='+this.subcatid+'&item=true&count=all&from=0')
     .subscribe(res => {
       this.results = res;
       this.subcategories = this.results.data;
-      console.log("subcat",this.subcategories);
+      // console.log("subcat",this.subcategories);
       this.subcategories.forEach(element => {
         element.cover_image_file = "http://13.127.87.1/uploads/" + element.images[0].path;
+      });
+    });
+  }
+
+  item(element){
+    console.log("item",element);
+    this.itemdata = element;
+    this.show = true;
+  }
+
+  lengths(index){
+    this.cindex = index;
+    this.product.categories[this.cindex].sub_categories.forEach(element => {
+      this.http.get('http://13.127.87.1/index.php/rest/items/get/shop_id/?shop_id='+this.productid+'&sub_cat_id='+element.id+'&item=true&count=all&from=0')
+      .subscribe(res => {
+        this.results = res;
+        element.length = this.results.data.length;
+        // console.log("result",this.results);
       });
     });
   }
